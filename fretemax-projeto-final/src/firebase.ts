997688100f251db9f6cb181de72844258cbfc24d
rodrigovtr38-hash <rodrigovtr38@gmail.com>
2026-05-04 12:04:18 -0300

    Update firebase.ts

--- fretemax-projeto-final/src/firebase.ts
+++ fretemax-projeto-final/src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage'; // ADICIONADO: Importando o Storage
 import firebaseConfig from '../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
@@ -8,3 +9,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 export const provider = new GoogleAuthProvider();
+export const storage = getStorage(app); // ADICIONADO: Exportando o Storage para o app
