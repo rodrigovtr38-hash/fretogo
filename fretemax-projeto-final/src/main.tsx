142eb33b0b0a82943dc1b9237bc01ee8679dc07a
rodrigovtr38-hash <rodrigovtr38@gmail.com>
2026-05-15 08:21:05 -0300

    Update main.tsx

--- fretemax-projeto-final/src/main.tsx
+++ fretemax-projeto-final/src/main.tsx
@@ -1,10 +1,24 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
 import App from './App.tsx';
 import './index.css';
+
+// REGISTRO DO SERVICE WORKER (PWA)
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    try {
+      await navigator.serviceWorker.register('/sw.js');
+
+      console.log('✅ FRETOGO PWA ativo');
+    } catch (error) {
+      console.error('❌ Erro ao registrar PWA:', error);
+    }
+  });
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
