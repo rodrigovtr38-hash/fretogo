53a0a9705647ccb1b0f5143c5130ab73c9ebcc30
rodrigovtr38-hash <rodrigovtr38@gmail.com>
2026-05-25 10:34:34 -0300

    Create useDriverRealtime.ts

---
+++ fretemax-projeto-final/src/hooks/useDriverRealtime.ts
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+
+import {
+  locationRealtimeService,
+} from '../services/locationRealtimeService';
+
+export const useDriverRealtime = (
+  driverId?: string,
+  isOnline?: boolean
+) => {
+  useEffect(() => {
+    if (!driverId) return;
+
+    if (isOnline) {
+      locationRealtimeService.start(
+        driverId
+      );
+    } else {
+      locationRealtimeService.stop();
+    }
+
+    return () => {
+      locationRealtimeService.stop();
+    };
+  }, [driverId, isOnline]);
+};
